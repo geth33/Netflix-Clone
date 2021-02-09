eab9ad2260bb1097890f4b55a440a06412cc0e64
geth33 <bryson.oar@hotmail.com>
2021-02-09 15:04:05 -0700

    Created clone's basic structure.

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCgPAX4bcZ6ppZq6mmFkByaZU2ohrQ7z_g",
+    authDomain: "netflix-clone-ba12f.firebaseapp.com",
+    projectId: "netflix-clone-ba12f",
+    storageBucket: "netflix-clone-ba12f.appspot.com",
+    messagingSenderId: "136061336655",
+    appId: "1:136061336655:web:8d06edde0b25b283d63e3a"
+  };
