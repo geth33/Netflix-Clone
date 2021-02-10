cd7631eaa1760b092d4eb8a027c8f0032019ad92
geth33 <bryson.oar@hotmail.com>
2021-02-09 19:01:24 -0700

    Added Navbar icons and structure.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,8 +3,12 @@ import "./css/Nav.css"
 
 function Nav() {
     return (
-        <div className='nav'>
-            <h1>This is the nav</h1>
+        <div className='nav nav__black'>
+            <div className="nav__contents">
+                <img className='nav__logo' src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"/>
+
+                <img className='nav__avatar' src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" />
+            </div>
         </div>
     )
 }
