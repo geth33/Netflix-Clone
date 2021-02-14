f49985e4c870e6ba3cea7bab2f835b44ac8d8c1e
geth33 <bryson.oar@hotmail.com>
2021-02-14 10:54:58 -0700

    Added Stripe payment page and payment options on profile edit screen.

--- src/views/ProfileScreen.js
+++ src/views/ProfileScreen.js
@@ -3,6 +3,7 @@ import Nav from "../components/Nav";
 import { useSelector } from "react-redux"; 
 import { selectUser } from "../features/userSlice";
 import './css/ProfileScreen.css';
+import PlansScreen from "./PlansScreen";
 import { auth } from '../firebase';
 
 function ProfileScreen() {
@@ -22,6 +23,7 @@ function ProfileScreen() {
                         <h2>{user.email}</h2>
                         <div className="profileScreen__plans">
                             <h3>Plans</h3>
+                            <PlansScreen />
                             <button 
                             onClick={() => auth.signOut()} 
                             className='profileScreen__signOut'>
